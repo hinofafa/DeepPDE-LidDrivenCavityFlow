4f768c27d857010b1bbd56b618adf2772d9773bf
hinofafa <hintkit@gmail.com>
2021-12-07 07:30:46 +0000

    save vtu files for Paraview analysis

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ The lid-driven cavity is a popular problem within the field of computational flu
 
 ## Problem Description
 
-The lid-driven cavity is a well-known benchmark problem for viscous incompressible fluid flow [75]. The geometry at stake is shown in Figure 27. We are dealing with a square cavity consisting of three rigid walls with no-slip conditions and a lid moving with a tangential x-direction velocity of 1m/s. The Reynolds number based on the cavity height is chosen be 10.
+The lid-driven cavity is a well-known benchmark problem for viscous incompressible fluid flow [[75]](https://www.sciencedirect.com/book/9781856176354/the-finite-element-method-for-fluid-dynamics). The geometry at stake is shown in Figure 27. We are dealing with a square cavity consisting of three rigid walls with no-slip conditions and a lid moving with a tangential x-direction velocity of 1m/s. The [Reynolds number](https://en.wikipedia.org/wiki/Reynolds_number) based on the cavity height is chosen be 10.
 
 <img src="pictures/lidrivencavity.PNG"  align='center'/>
 
-## Methodology
+
+## Steps (Modulus)
+1. - Geometry setup (Constructive Solid Geometry, CSG or STL)
+   - Sampling on boundaries and interior regions
+2. Equation setup (TrainDomain)
+3. Solver setup (Solver)
 
 <img src="pictures/Methodology.PNG"  align='center'/>
 
--- ldc_2d.py
+++ ldc_2d.py
@@ -13,9 +13,14 @@ height = 0.1
 width = 0.1
 vel = 1.0
 
-# define geometry
+# define geometry (x y coordinates of bottom left and top right corners)
 rec = Rectangle((-width / 2, -height / 2), (width / 2, height / 2))
 geo = rec
+
+# visualize geometry by Paraview after saving .vtu point cloud file
+# from modulus.plot_utils.vtk import var_to_vtk
+# samples = geo.sample_boundary(1000)
+# var_to_vtk(samples, './geo')
 
 # define sympy varaibles to parametize domain curves
 x, y = Symbol('x'), Symbol('y')
@@ -25,6 +30,11 @@ class LDCTrain(TrainDomain):
     super(LDCTrain, self).__init__()
 
     #top wall
+    # boundary_bc: sample the boundary
+    # outvar_sympy: define variables as key in dict
+    # batch_size_per_area: # points
+    # lambda_sympy: for point between top and other wall who get sharp discontinue velocity, Signed Distance Field
+    # criteria: points that y coor == height/2 (top)
     topWall = geo.boundary_bc(outvar_sympy={'u': vel, 'v': 0},
                               batch_size_per_area=10000,
                               lambda_sympy={'lambda_u': 1.0 - 20 * Abs(x),  # weight edges to be zero
@@ -49,7 +59,7 @@ class LDCTrain(TrainDomain):
     self.add(interior, name="Interior")
 
 
-# validation data
+# validation data, Data Driven from OpenFOAM
 mapping = {'Points:0': 'x', 'Points:1': 'y', 'U:0': 'u', 'U:1': 'v', 'p': 'p'}
 openfoam_var = csv_to_dict('openfoam/cavity_uniformVel0.csv', mapping)
 openfoam_var['x'] += -width / 2  # center OpenFoam data
@@ -70,14 +80,20 @@ class LDCSolver(Solver):
 
   def __init__(self, **config):
     super(LDCSolver, self).__init__(**config)
+    # solve continuity equation and momentum equations in x, y axis
+    # self.equations = {}
+    # set equations in dict
+    # set_equations['continuity'] = rho.diff(t) + (rho*u).diff(x) + (rho*v).diff(y) + (rho*w).diff(z)
     self.equations = NavierStokes(nu=0.01, rho=1.0, dim=2,
                                   time=False).make_node()
+    # default netowrk 6 hidden layers with 512 nodes
     flow_net = self.arch.make_node(name='flow_net',
                                    inputs=['x', 'y'],
                                    outputs=['u', 'v', 'p'])
     self.nets = [flow_net]
 
   @classmethod
+  # used to edit default network setting
   def update_defaults(cls, defaults):
     defaults.update({
         'network_dir': './network_checkpoint_ldc_2d',
